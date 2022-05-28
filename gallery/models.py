7890fd90c1b567ea02b6bd6406dcf8d473da73f1
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-29 02:13:22 +0300

    Feat : Adds category and locations models respectively

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
+    @classmethod
+    def display_all_categories(cls):
+        return cls.objects.all()
+
+class Location(models.Model):
+    loc = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.loc
+        
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def update_location(cls,id,name):
+        cls.objects.filter(id = id).update(loc = name)
+
+    @classmethod
+    def display_all_locations(cls):
+        return cls.objects.all()
+
+
