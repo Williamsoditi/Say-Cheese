2bd972bd1d161efb3522d23d44fc48b755dd4ab8
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-29 08:54:03 +0300

    Chore : Adds CategoryTestClass - This adds tests to define the category

--- gallery/tests.py
+++ gallery/tests.py
@@ -32,3 +32,32 @@ class LocationTestClass(TestCase):
         self.strathmore.update_location(self.strathmore.id,'alice')
         update = Location.objects.get(locate = 'alice')
         self.assertTrue(update.locate, 'alice')
+
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.sports = Category(name = 'sports')
+    
+    def test_save_category(self):
+        self.sports.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+    def test_delete_category(self):
+        self.sports.save_category()
+        categories= Category.objects.all()
+        self.assertEqual(len(categories),1)
+        self.sports.delete_category()
+        categories=Category.objects.all()
+        self.assertEqual(len(categories),0)
+
+    def test_update_category(self):
+        self.sports.save_category()
+        self.sports.update_category(self.sports.id,'alice')
+        update = Category.objects.get(name = "alice")
+        self.assertTrue(update.name, 'alice')
+
+    def test_display_categories(self):
+        self.sports.save_category()
+        self.assertEqual(len(Category.display_all_categories()),1)
