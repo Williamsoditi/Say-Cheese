370a685adda9a037af621bd28b53e7d919630342
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-29 09:59:31 +0300

    Feat : Adds media storage configurations - This configures the media paths

--- Say_cheese/settings.py
+++ Say_cheese/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -137,6 +138,9 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,4 +1,6 @@
-from django.urls import re_path as url,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path as url
 from . import views
 
 urlpatterns=[
@@ -6,3 +8,6 @@ urlpatterns=[
     url(r'^pictures/',views.pictures,name ='pictures'),
     url(r'^search/', views.search_results, name='search_results'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
